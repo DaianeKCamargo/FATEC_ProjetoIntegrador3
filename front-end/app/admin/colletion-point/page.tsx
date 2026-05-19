6f50325fd964199f1558ee7edda41a132f188ff2
BOLZIRI <nicolebolzani2@gmail.com>
2026-05-18 21:08:08 -0300

    feat: estrutura front

---
+++ front-end/app/admin/colletion-point/page.tsx
@@ -0,0 +1,6 @@
+// aqui criar o menu com as opções de cadastro de ponto de coleta
+
+// aprovar ou recusar cadastro que vier do formulários
+
+// visualizar todos os cadastros, tanto os recusados quanto os aprovados eeeeeeeeeeeeeee 
+// editar os que já estão aprovados
---
+++ front-end/app/user/colletion-point/page.tsx
@@ -0,0 +1,5 @@
+// API própria do que já tem cadastrado
+
+// API do mapa
+
+// mostrar descrição dos pontos de coleta de acordo com a pesquisa
