71a6237ead7a014328ad0379469cb46a0f3eddc8
Mariaa <dudamlopes10@gmail.com>
2026-05-19 20:13:30 -0300

    código
    primeira parte do código "about-us" e "how-donate"

---
+++ front-end/app/user/about-us/page.tsx
@@ -0,0 +1,117 @@
+ 'use client';
+
+import Titulo from '@/components/titulo/Titulo';
+import style from '@/styles/sobre-nos.module.css';
+import Image from 'next/image';
+
+import { PawPrint, Recycle, Info} from "lucide-react";
+
+export default function SobreNos() {
+    return (
+        <section className={style.page}>
+            <Titulo 
+                src="/img_titulo_verde.png" 
+                title="Sobre Nós" 
+                label="Não conhece nosso projeto? Você está no lugar certo!"
+            />
+
+            <div className={style.sobre}>
+                
+                {/* ---------------------- QUEM SOMOS ---------------------- */}
+                <div className={style.sectionBox}>
+                    <div className={style.sectionHeader}>
+                        <PawPrint size={28} />
+                        <h2>Quem Somos</h2>
+                    </div>
+
+                    <div className={style.sectionContent}>
+                        <p>
+                           Diante da realidade dos animais abandonados e maltratados, 
+                           o <strong>Projeto TamPets</strong> busca contribuir para o controle da superpopulação animal por meio da reciclagem de tampinhas PET, 
+                           destinando os recursos a castrações. 
+                        </p>
+
+                        <p>
+                            Embora existam iniciativas voltadas à causa animal em Sorocaba e região, o fator financeiro 
+                            ainda impede que muitos tutores, abrigos e protetores independentes tenham acesso à castração.
+                        </p>
+
+                        <p>
+                            Com base nisso, o <strong>Projeto TamPets</strong> tem como objetivo <strong>coletar e vender tampinhas plásticas</strong>, 
+                            criando um fundo destinado exclusivamente à realização dessas cirurgias para animais em situações 
+                            de vulnerabilidade.
+                        </p>
+
+                        <p>
+                            A castração evita abandono, maus-tratos, fome, ninhadas indesejadas e diversas doenças.
+                        </p>
+
+                        <Image
+                            src={"/TamPets-bannerzin.png"}
+                            alt="TamPets Banner"
+                            width={500}
+                            height={300}
+                            className={style["Image-banner"]}
+                        />
+                    </div>
+                </div>
+
+                {/* ---------------------- O QUE FAZEMOS ---------------------- */}
+                <div className={style.sectionBox}>
+                    <div className={style.sectionHeader}>
+                        <Recycle size={28} />
+                        <h2>O Que Fazemos</h2>
+                    </div>
+
+                    <div className={style.sectionContent}>
+                        <p><strong>Uma iniciativa independente em prol dos animais.</strong></p>
+
+                        <p>
+                            A coleta das tampinhas acontece por meio de parcerias com pessoas físicas e jurídicas 
+                            que apoiam voluntariamente o projeto.
+                        </p>
+
+                        <p>
+                            O peso total arrecadado mensalmente é divulgado nas redes sociais do projeto.
+                        </p>
+
+                        <p>
+                            Todos os recursos obtidos com a venda das tampinhas são destinados <strong>direta e exclusivamente</strong> 
+                            aos responsáveis pelos atendimentos — ONGs, abrigos e clínicas veterinárias parceiras.
+                        </p>
+
+                        <p>
+                            O sucesso desse trabalho depende do engajamento da população de Sorocaba e região, que pode colaborar 
+                            coletando tampinhas e entregando-as nos pontos de coleta parceiros.
+                        </p>
+                    </div>
+                </div>
+
+                {/* ---------------------- IMPORTANTE SABER ---------------------- */}
+                <div className={style.sectionBox}>
+                    <div className={style.sectionHeader}>
+                        <Info size={28} />
+                        <h2>Importante Saber</h2>
+                    </div>
+
+                    <div className={style.sectionContent}>
+                        <ul>
+                            <li>O <strong>Projeto TamPets não é uma ONG</strong>;</li>
+                            <li><strong>Não possui vínculos políticos</strong>;</li>
+                            <li><strong>Não possui abrigo</strong>;</li>
+                            <li><strong>Não realiza resgates</strong>;</li>
+                            <li><strong>Não promove feiras de adoção ou mutirões de castração</strong> — apenas apoia através da divulgação;</li>
+                            <li><strong>Não aceita doações em dinheiro</strong>, exceto quando realizadas <strong>diretamente à clínica parceira</strong>.</li>
+                        </ul>
+
+                        <p>
+                            <strong>Eliada Marcos (Lia)</strong><br />
+                            Idealizadora do Projeto TamPets
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    );
+}
---
+++ front-end/app/user/how-donate/page.tsx
@@ -0,0 +1,136 @@
+'use client'
+import React from 'react';
+import Image from 'next/image';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Titulo from '@/components/titulo/Titulo';
+import style from '@/styles/como-doar.module.css';
+import { TriangleAlert, Bone } from 'lucide-react';
+
+export default function ComoDoar() {
+  return (
+    <section className={style.page}>
+      <Titulo 
+        src="/img_titulo_amarelo.png" 
+        title="Como doar" 
+        label=" Está com duvída de como você pode nos ajudar?"/>
+
+    <div className={style.doar}>
+      <div>
+        {/* não utiliza esse formato de style no className, conforme conversamos utilizar o module, já estou deixando importado aqui */}
+        <header>
+          <div className={style.sectionBox}>
+            <div className={style.sectionHeader}>
+              <Bone size={28}/>
+              <h2>Como faço para doar?</h2>
+            </div>
+            <div className={style.sectionContent}>
+              <p>
+                Junte suas tampinhas e procure o ponto de coleta mais próximo de você!
+              </p>
+
+              <p>
+                Não conhece nenhum ponto de coleta?
+                Procure um na nossa aba Ponto de coleta.
+              </p>
+            </div>
+          </div>
+        </header>
+
+        <div className={style.sectionBox}>
+          <div className={style.sectionHeader}>
+             <TriangleAlert size={28} />
+            <h2>ATENÇÃO!</h2>
+          </div>
+
+          <div className={style.sectionContent}>
+            <p>Para facilitar a triagem das tampinhas, não coloque junto delas:</p>
+            <ul>
+              <li>- Itens não recicláveis</li>
+              <li>- Lacres</li>
+              <li>- Pregos, parafusos, etc.</li>
+              <li>- Plásticos que não sejam tampinhas</li>
+              <li>- Vidros</li>
+              <li>- Cartela de remédio</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={style.sectionBox}>
+          <div className={style.sectionHeader}>
+            <h2>Que tipo de tampinhas aceitamos:</h2>
+          </div>
+
+          <div className={style.sectionContent}>
+            <h3>Alimentos</h3>
+            <Image
+                src={"/Tampinhasplasticas.jpg"} 
+                alt={'tampinhas plasticas'}
+                width={250}
+                height={400}
+                className={style["Image-banner"]}/>
+
+            <p>Tampas de refrigerantes, sucos, margarina, maionese, molhos, 
+            requeijão, óleo de cozinha etc.</p>
+          </div>
+          <div className={style.sectionContent}>
+            <h3>Produtos de limpeza</h3>
+            <Image
+                src={"/ProdLimpeza.jpeg"} 
+                alt={'Produtos de limpeza'}
+                width={280}
+                height={200}
+                className={style["Image-banner"]}/>
+            <p>Tampas de amaciante, cloro, cândida, desinfetante, desengordurante, etc.</p>
+          </div>
+          <div className={style.sectionContent}>
+            <h3>Produtos de Higiene</h3>
+            <Image
+                  src={"/ProdHigiene.jpg"} 
+                  alt={'Produtos de higiene'}
+                  width={280}
+                  height={200}
+                  className={style["Image-banner"]}/>
+            <p>Tampas de shampoo, condicionador, cremes,
+            pasta de dentes, sabonete líquido, hidratantes, etc.</p>
+          </div>
+        </div>
+
+        <div className={style.sectionBox}>
+          <div className={style.sectionHeader}>
+            <h2>O que a Tampets FAZ e NÃO FAZ</h2>
+          </div>
+
+          <div className={style.sectionContent}>
+            <h3>FAZ</h3>
+              <ul>
+                <li><i className="bi bi-check2-circle">Divulga feirinhas de adoção</i></li>
+                <li><i className="bi bi-check2-circle">Divulga pedidos de tutores cujo animais fugiram </i></li>
+                <li><i className="bi bi-check2-circle">Divulga animais que foram encontrados e precisam voltar para casa</i></li>
+                <li><i className="bi bi-check2-circle">Divulga campanhas de castração realizadas pelas prefeituras da região</i></li>
+                <li><i className="bi bi-check2-circle"
+                >Divulga eventos beneficientes de arrecadação de dinheiro para ajudar abrigos, protetores ou ONGs
+                </i></li>
+              </ul>
+          </div>
+
+          <div className={style.sectionContent}>
+            <h3>NÃO FAZ</h3>
+              <ul> 
+                <li><i className="bi bi-x-circle">Não indica clínicas veterinárias</i></li>
+                <li><i className="bi bi-x-circle">Não tem abrigo</i></li>
+                <li><i className="bi bi-x-circle">Não faz rifas</i></li>
+                <li><i className="bi bi-x-circle">Não divulga pedidos de pix ou transfência de valores</i></li>
+                <li><i className="bi bi-x-circle">Não paga castrações de animais de raça ou de filhotes</i></li>
+                <li><i className="bi bi-x-circle">Não indica abrigos, lar temporário ou ONGs que resgatam animais</i></li>
+                <li><i className="bi bi-x-circle">Não troca tampinhas por ração</i></li>
+                <li><i className="bi bi-x-circle">Não troca tampinhas por castrações</i></li>
+                <li><i className="bi bi-x-circle">Não paga tratamentos veterinários</i></li>
+                <li><i className="bi bi-x-circle">Não divulga nas redes sociais animais para adoção</i></li>
+              </ul> 
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
+}
