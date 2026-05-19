7a4770ff426b6fdc419868fb51d9d22d33c82d9b
Sabrina Sant'Ana <sah.silva1d29@gmail.com>
2026-05-18 21:52:03 -0300

    Comentando ações futuras

---
+++ front-end/app/user/news/page.tsx
@@ -0,0 +1,4 @@
+// Criar aqui a página de exibição das notícias (TamPets na Mídia)
+// Fazer a ligação com o banco de dados
+// Fazer a página responsiva e de acordo com o atual design do projeto
+// GET (Apresentar as notícias)
