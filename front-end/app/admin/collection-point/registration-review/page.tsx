e3a2ee6a84caf659be5523306e1aba054f294d18
DaianeKCamargo <dkeelly@gmail.com>
2026-05-26 09:39:25 -0300

    feat: REGISTRATION-REVIEW
    Código apenas para deploy.

---
+++ front-end/app/admin/collection-point/registration-review/page.tsx
@@ -0,0 +1,8 @@
+export default function RegistrationReview() {
+    return (
+        <div>
+            <h1>Registration Review</h1>
+            <p>This is a simple registration review page.</p>
+        </div>
+    );
+}
