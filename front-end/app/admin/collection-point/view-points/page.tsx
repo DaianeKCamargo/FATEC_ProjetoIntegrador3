4e92e0b0b0929d37ce48e101374714f229bc3307
DaianeKCamargo <dkeelly@gmail.com>
2026-05-26 09:44:19 -0300

    feat: View-Point (Collection-point)
    Código apenas teste para deploy no versel.

---
+++ front-end/app/admin/collection-point/view-points/page.tsx
@@ -0,0 +1,8 @@
+export default function RegistrationReview() {
+    return (
+        <div>
+            <h1>View Collection Points</h1>
+            <p>This is a simple page to view collection points.</p>
+        </div>
+    );
+}
